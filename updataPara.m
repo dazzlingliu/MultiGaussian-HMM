1ad8b2579ab2b8f043d9c75d54b7ebcf57110d1f
dazzlingliu <38676954+dazzlingliu@users.noreply.github.com>
2020-03-21 11:58:54 +0800

    Update updataPara.m

--- updataPara.m
+++ updataPara.m
@@ -8,58 +8,42 @@ D = options.D;
 ModParaNew = struct();
 ModParaNew.Pi = zeros(1,K);
 ModParaNew.A = zeros(K,K);
-
-if options.updaterest == 1
-    % Pi
-    sPi = zeros(tN,K);
-    for n = 1:tN
-        for i = 1:K
-            sPi(n,i) = gamma((n-1)*t0+1,i);
-        end   
-    end  
-    if tN == 1
-        ModParaNew.Pi = sPi;
-    else
-        ModParaNew.Pi = mean(sPi);
-    end
-    % A
+% Pi
+sPi = zeros(tN,K);
+for n = 1:tN
     for i = 1:K
-        for j = 1:K
-            ModParaNew.A(i,j) = sum(kersi(i,j,:))/sum(gamma(:,i));
-        end
-    end
-    % mu
-    for i = 1:K
-        ModParaNew.state(i).mu = (gamma(:,i)'*Y)/sum(gamma(:,i));
-    end
-
+        sPi(n,i) = gamma((n-1)*t0+1,i);
+    end   
+end  
+if tN == 1
+    ModParaNew.Pi = sPi;
 else
-    ModParaNew.Pi = ModPara.Pi;
-    ModParaNew.A = ModPara.A;
-    for i = 1:K
-        ModParaNew.state(i).mu = ModPara.state(i).mu;
+    ModParaNew.Pi = mean(sPi);
+end
+% A
+for i = 1:K
+    for j = 1:K
+        ModParaNew.A(i,j) = sum(kersi(i,j,:))/sum(gamma(:,i));
     end
 end
+% mu
+for i = 1:K
+    ModParaNew.state(i).mu = (gamma(:,i)'*Y)/sum(gamma(:,i));
+end
+
 
 % sigma
-if options.updatesigma == 1
-   for i = 1:K
-        mu = ModParaNew.state(i).mu;   
-        sigmaT = zeros(D,D);
-        for t = 1:t0*tN
-            midsigmaT = (Y(t,:)-mu)'*(Y(t,:)-mu);
-            sigmaT = sigmaT + gamma(t,i)*midsigmaT;
-        end
-        ModParaNew.state(i).sigma = sigmaT/sum(gamma(:,i));% + 0.01*eye(D);
-    end
 
-elseif options.updatesigma == 0
-    % sigma don't update
-    for i = 1:K
-        ModParaNew.state(i).sigma = ModPara.state(i).sigma;
+for i = 1:K
+    mu = ModParaNew.state(i).mu;   
+    sigmaT = zeros(D,D);
+    for t = 1:t0*tN
+        midsigmaT = (Y(t,:)-mu)'*(Y(t,:)-mu);
+        sigmaT = sigmaT + gamma(t,i)*midsigmaT;
     end
-
+    ModParaNew.state(i).sigma = sigmaT/sum(gamma(:,i));% + 0.01*eye(D);
 end
+
 
 end
 
